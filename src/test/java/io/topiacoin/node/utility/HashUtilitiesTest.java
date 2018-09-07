6d699e6167edad6abc3b759de7c47288311203f5
John Haager <jhaager@topiatechnology.com>
2018-09-07 09:41:40 -0700

    SDFS-314 - Created the interface for Data Storage Manager. SDFS-315 - Started Implementation of the Data Storage Manager.

--- src/test/java/io/topiacoin/node/utility/HashUtilitiesTest.java
+++ src/test/java/io/topiacoin/node/utility/HashUtilitiesTest.java
@@ -1,14 +1,30 @@
 package io.topiacoin.node.utility;
 
+import io.topiacoin.node.utilities.HashUtilities;
 import org.junit.Test;
+
+import java.util.Random;
 
 import static junit.framework.TestCase.*;
 
 public class HashUtilitiesTest {
 
-    @Test
-    public void sanityTest() {
-        fail ( "I'm the sanest test ever!");
-    }
+//    @Test
+//    public void sanityTest() {
+//        fail ( "I'm the sanest test ever!");
+//    }
 
+    @Test
+    public void testHashGenerationAndVerificationOfByteArray() throws Exception {
+        String algorithm = "SHA-256";
+        byte[] data = new byte[1024] ;
+        Random random = new Random();
+        random.nextBytes(data);
+
+        String dataHash = HashUtilities.generateHash(algorithm, data);
+        assertNotNull ( "No Hash String was Returned", dataHash);
+
+        boolean matches = HashUtilities.verifyHash(dataHash, data);
+        assertTrue ( "Data did not match the Hash", matches);
+    }
 }
