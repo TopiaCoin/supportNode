d905f7142c8eddfdd3b444c3f4869f24589c3476
John Haager <jhaager@topiatechnology.com>
2018-09-07 10:28:13 -0700

    SDFS-315 - Extended the Hash Utilities test to test the stream side of the code.  Moved MemoryDataStorageProvider class out of the test cases and into the code side.  Added the AbstractThrowableTest and individual test cases for all of the exceptions.

--- src/test/java/io/topiacoin/node/utility/HashUtilitiesTest.java
+++ src/test/java/io/topiacoin/node/utility/HashUtilitiesTest.java
@@ -3,6 +3,7 @@ package io.topiacoin.node.utility;
 import io.topiacoin.node.utilities.HashUtilities;
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
 import java.util.Random;
 
 import static junit.framework.TestCase.*;
@@ -25,6 +26,23 @@ public class HashUtilitiesTest {
         assertNotNull ( "No Hash String was Returned", dataHash);
 
         boolean matches = HashUtilities.verifyHash(dataHash, data);
+        assertTrue ( "Data did not match the Hash", matches);
+    }
+
+    @Test
+    public void testHashGenerationAndVerificationOfDataStream() throws Exception {
+        String algorithm = "SHA-256";
+        byte[] data = new byte[1024] ;
+        Random random = new Random();
+        random.nextBytes(data);
+
+        ByteArrayInputStream dataStream = new ByteArrayInputStream(data) ;
+
+        String dataHash = HashUtilities.generateHash(algorithm, dataStream);
+        assertNotNull ( "No Hash String was Returned", dataHash);
+
+        dataStream.reset();
+        boolean matches = HashUtilities.verifyHash(dataHash, dataStream);
         assertTrue ( "Data did not match the Hash", matches);
     }
 }
